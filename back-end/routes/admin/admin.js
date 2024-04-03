dd1522df8ab821c3623557ebc47b554889bfd771
Nadini Annisa Byant <2111522021_nadini@student.unand.ac.id>
2024-04-04 00:42:20 +0700

    progress 2

--- back-end/routes/admin/admin.js
+++ back-end/routes/admin/admin.js
@@ -2,5 +2,10 @@ const express = require('express')
 const router = express.Router()
 const middleware = require('../../middleware/authentication')
 const controllers = require('../../controllers/admin/admin')
+const middleware = require('../../middleware/authentication')
+
+router.post('/tambahAdmin', controllers.tambahAdmin)
+router.post('/loginAdmin', controllers.loginAdmin)
+router.delete('/logoutAdmin', middleware.verifyTokenAdmin, controllers.logoutAdmin)
 
 module.exports = router
