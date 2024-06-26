a72ac151c5eb08cf4ff8f704f0b7341336addedd
Nadini Annisa Byant <2111522021_nadini@student.unand.ac.id>
2024-06-26 20:58:54 +0700

    hapus

--- front-end/src/components/button/JenisSurat.jsx
+++
@@ -1,9 +0,0 @@
-export default function JenisSurat ({ label, id, onClick }) {
-    return (
-        <>
-            <button className="bg-secondary w-fit rounded-2xl" type="button" onClick={onClick}>
-                <p className="font-extrabold text-2xl text-white p-5" id={id}>{label}</p>
-            </button>
-        </>
-    )
-}
--- front-end/src/pages/Admin/LoginAdmin.jsx
+++
@@ -1,9 +0,0 @@
-import FormLogin from "../../components/form/FormLogin"
-
-export default function LoginAdmin (){
-    return (
-        <>
-        <FormLogin label1={'username'} label2={'password'} gambar={'loginAdmin.svg'}/>
-        </>
-    )
-} 
